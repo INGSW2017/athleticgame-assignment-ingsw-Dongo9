1ecae49dc7727111bcb3f5c113170ac72634d4fd
Dongo9 <futura95@live.it>
2017-10-18 13:13:25 +0200

    Esercizio completo

--- AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
+++ AthleticGameAssignmentINGSW/test/it/unical/test/AthleticGameBasicTest.java
@@ -1,5 +1,35 @@
 package it.unical.test;
 
+import java.time.Instant;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import it.unical.asde.AthleticGame;
+
 public class AthleticGameBasicTest {
 
+	private static AthleticGame game;
+	
+	@BeforeClass
+	public static void init()
+
+	{
+		game=new AthleticGame("test");
+	}
+	
+	@Test
+	public void addArrivalTest()
+	{
+		game.addArrival("Stincio",Instant.now());
+		Assert.assertEquals(1,game.getArrivals().size());
+	}
+	
+	@Test(expected= IllegalArgumentException.class)
+	public void getParecipiantTimeTest()
+	{
+		game.getParecipiantTime("Carlo");
+	}
+	
 }
